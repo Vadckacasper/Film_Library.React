c2046a6ddf790ded58a9c1d7e613bade6329efa6
Kalinin_Vadim <47732165+Vadckacasper@users.noreply.github.com>
2022-08-04 17:11:46 +0700

    Changed data models, added anotation

--- Film_Library.React/Models/Actor.cs
+++ Film_Library.React/Models/Actor.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +8,8 @@ namespace Film_Library.React.Models
 {
     public class Actor
     {
+        [Key]
+        public int Id { get; set; }
         public string Name { get; set; }
         public string Surname { get; set; }
         public string Path_Img { get; set; }
--- Film_Library.React/Models/Film.cs
+++ Film_Library.React/Models/Film.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +8,7 @@ namespace Film_Library.React.Models
 {
     public class Film
     {
+        [Key]
         public int Id { get; set; }
         public string Name { get; set; }
         public string ShortDescription { get; set; }
