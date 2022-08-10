679c56c6d31409928dad6ffa91cba8d43a4e37fb
Kalinin_Vadim <47732165+Vadckacasper@users.noreply.github.com>
2022-08-10 11:21:52 +0700

    Layout of the actor's card

--- Film_Library.React/ClientApp/src/components/CardActor/CardActor.js
+++ Film_Library.React/ClientApp/src/components/CardActor/CardActor.js
@@ -1,15 +1,12 @@
 import React from "react";
+import "./CardActor.css";
 
 export function CardActor(props) {
-    const Actors = props.actorCard;
+  const Actors = props.actorCard;
   return (
-    <div className="card">
-    <img src={Actors.pathImg} className="card-img-top" alt="..."/>
-    <div className="card-foter">
-      <h5 className="card-title">{Actors.name} {Actors.surname}</h5>
-      <p className="card-text">Небольшой пример текста, который должен основываться на названии карточки и составлять основную часть содержимого карты.</p>
-      <a href="#" className="btn btn-primary">Перейти куда-нибудь</a>
-    </div>
-  </div>
+        <div className="card-actor">
+          <img src={Actors.path_Img} className="img img-responsive" />
+          <div className="actor-name">{Actors.fullName}</div>
+        </div>
   );
 }
