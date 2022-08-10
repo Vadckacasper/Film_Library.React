7a2ac8d36430cfb1d4f5f2d8a3055eee80d5e174
Kalinin_Vadim <47732165+Vadckacasper@users.noreply.github.com>
2022-08-10 10:55:00 +0700

    new ViewModel for displaying data

---
+++ Film_Library.React/ViewModel/CardActorViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Film_Library.React.ViewModel
+{
+    public class CardActorViewModel
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public string DateBirth { get; set; }
+        public string PlaceBirth { get; set; }
+        public string Path_Img { get; set; }
+
+    }
+}
