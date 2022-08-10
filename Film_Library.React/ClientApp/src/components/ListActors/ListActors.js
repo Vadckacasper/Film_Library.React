730651e03e5d5c814ac3b78e7f67b4b11ca1f10e
Kalinin_Vadim <47732165+Vadckacasper@users.noreply.github.com>
2022-08-10 11:20:05 +0700

    A request to the controller has been added and processed. The layout is done and react/swiper is used

--- Film_Library.React/ClientApp/src/components/ListActors/ListActors.js
+++ Film_Library.React/ClientApp/src/components/ListActors/ListActors.js
@@ -1,12 +1,64 @@
 import React from "react";
+import {CardActor} from "../CardActor/CardActor"
+import { useState, useEffect } from "react";
+import {Swiper, SwiperSlide } from "swiper/react";
+import { Pagination } from "swiper";
+import 'swiper/swiper-bundle.min.css';
+import "./ListActors.css"
 
 export function ListActors(props) {
-    const Actors = props.actorsData;
-    console.log(Actors);
+    const [ActorsData, setActorsData] = useState([]);
 
+    useEffect(() => {
+      load();
+    },[props.filmId]);
+
+    const load = async () => {
+      const response = await fetch(`api/Actors/_actors/${props.filmId}`);
+      if (response.ok) {
+        const data = await response.json();
+        setActorsData(data);
+      }
+    };
+
+    const renderActorsData = (actors) => {
+      return actors.map((actor, key) => (
+          <SwiperSlide key={key}><CardActor actorCard={actor} /></SwiperSlide>
+      ));
+    }
 
   return (
-    <p>{console.log(Actors)}</p>
+      <Swiper 
+      slidesPerView={5}
+      breakpoints={{
+        "320": {
+          slidesPerView: 1,
+        },
+        "480": {
+          slidesPerView: 2,
+        },
+        "768": {
+          slidesPerView: 3,
+        },
+        "1024": {
+          slidesPerView: 4,
+        },
+        "1440": {
+          slidesPerView: 5,
+        },
+        "1900": {
+          slidesPerView: 6,
+        },
+      }}
+        spaceBetween={10}
+        freeMode={true}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Pagination]}
+        className="mySwiper">
+      {renderActorsData(ActorsData)}
+      </Swiper>
   );
 }
 
