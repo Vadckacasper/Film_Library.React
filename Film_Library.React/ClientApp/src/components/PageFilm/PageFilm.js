7e333c7c627ad78be28fc2873cc7c4cd5fab7141
Kalinin_Vadim <47732165+Vadckacasper@users.noreply.github.com>
2022-08-10 11:17:45 +0700

    The fetch request has been changed, the layout has been modified

--- Film_Library.React/ClientApp/src/components/PageFilm/PageFilm.js
+++ Film_Library.React/ClientApp/src/components/PageFilm/PageFilm.js
@@ -8,69 +8,52 @@ export function PageFilm() {
   const [filmData, setFilmData] = useState(0);
 
   const params = useParams();
-  const prodId = params.id;
-  console.log(prodId);
+  const filmId = params.id;
 
   useEffect(() => {
-    console.log("Всем привет")
     load();
-  },[]);
+  }, []);
 
   const load = async () => {
-    const response = await fetch(`api/Films/${prodId}`);
-    console.log(response);
+    const response = await fetch(`api/Films/${filmId}`);
     if (response.ok) {
       const data = await response.json();
-      console.log(data);
       setFilmData(data);
     }
   };
-  const Table = () =>{
-    return(
+  const Table = () => {
+    return (
       <table className="table table-borderless">
-      <tbody>
-        <tr>
-          <th>Жанр</th>
-          <td>{filmData.name}</td>
-        </tr>
-        <tr>
-          <th>Страны</th>
-          <td>{filmData.countries}</td>
-        </tr>
-        <tr>
-          <th>Режисер</th>
-          <td>{filmData.producer}</td>
-        </tr>
-        <tr>
-          <th>Год производства</th>
-          <td>{filmData.yearProduction}</td>
-        </tr>
-      </tbody>
-    </table>
-    )
+        <tbody>
+          <tr>
+            <th>Жанр</th>
+            <td>{filmData.name}</td>
+          </tr>
+          <tr>
+            <th>Страны</th>
+            <td>{filmData.countries}</td>
+          </tr>
+          <tr>
+            <th>Режисер</th>
+            <td>{filmData.producer}</td>
+          </tr>
+          <tr>
+            <th>Год производства</th>
+            <td>{filmData.yearProduction}</td>
+          </tr>
+        </tbody>
+      </table>
+    );
   };
 
-  const renderActorsData = (actors) => {
-    return actors.map((actor, key) => (
-        <p key={key}>{actor.name}</p>
-    ));
-  }
-
   return (
-        <div className="row">
-          <div className="col-lg-6">
-            <img
-              className="img-fluid img-film"
-              src={filmData.pathImg}
-            />
-          </div>
-          <div className="col-lg-6 table-data">
-              {Table()}
-          </div>
-          <div className="pt-4">
-          {filmData.fullDescription}
-          </div>
-          <p>{console.log(filmData.actors.lenght)}</p>
-        </div>
+    <div className="row">
+      <div className="col-lg-6">
+        <img className="img-fluid img-film" src={filmData.pathImg} />
+      </div>
+      <div className="col-lg-6 table-data">{Table()}</div>
+      <div className="container pt-4">{filmData.fullDescription}</div>
+      <ListActors filmId={filmId} />
+    </div>
   );
 }
